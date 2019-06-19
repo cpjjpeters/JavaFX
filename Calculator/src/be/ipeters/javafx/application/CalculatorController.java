4e4c7f47593ace3afa56ea391cdb30df70a91a81
cpjjpeters <cpjjpeters@gmail.com>
2019-06-19 22:38:39 +0200

    remove unneeded stuff

--- Calculator/src/be/ipeters/javafx/application/CalculatorController.java
+++ Calculator/src/be/ipeters/javafx/application/CalculatorController.java
@@ -11,43 +11,31 @@ import javafx.scene.control.TextField;
 
 public class CalculatorController implements Initializable {
 	@FXML
-	   private Button addButton;
-	  
-	   @FXML
-	   private TextField firstNumberTextField,secondNumberTextField, resultTextField;
-	   @FXML
-	   private void handleAdditionAction(ActionEvent event) {
-		   double first = Double.parseDouble(firstNumberTextField.getText());
-		   double second = Double.parseDouble(secondNumberTextField.getText());
-		   double result = first + second;
-		   resultTextField.setText(String.valueOf(result));
-	   }
-	
-	   @FXML
-	   private void handleMultiplicationAction(ActionEvent event) {
-		   double first = Double.parseDouble(firstNumberTextField.getText());
-		   double second = Double.parseDouble(secondNumberTextField.getText());
-		   double result = first * second;
-		   resultTextField.setText(String.valueOf(result));
-	   }
-	   
+	private Button addButton;
+
+	@FXML
+	private TextField firstNumberTextField, secondNumberTextField, resultTextField;
+
+	@FXML
+	private void handleAdditionAction(ActionEvent event) {
+		double first = Double.parseDouble(firstNumberTextField.getText());
+		double second = Double.parseDouble(secondNumberTextField.getText());
+		double result = first + second;
+		resultTextField.setText(String.valueOf(result));
+	}
+
+	@FXML
+	private void handleMultiplicationAction(ActionEvent event) {
+		double first = Double.parseDouble(firstNumberTextField.getText());
+		double second = Double.parseDouble(secondNumberTextField.getText());
+		double result = first * second;
+		resultTextField.setText(String.valueOf(result));
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	// When user click on myButton
-	   // this method will be called.
-	   public void doAdd(ActionEvent event) {
-	       System.out.println("Button Clicked!");
-	      
-	       
-	      
-	      
-	        
-	        // Show in VIEW
-	       resultTextField.setText("output");
-	      
-	   }
 
 }
